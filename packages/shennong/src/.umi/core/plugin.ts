a257ea6d746a409b0d40a6116e6143eff699221d
kyu <qq950810@gmail.com>
2020-10-23 00:28:47 +0800

    feat(monorepo): init

---
+++ packages/shennong/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/qiuqi/Documents/github/kyuch4n.github.io/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','request',],
+});
+
+export { plugin };
