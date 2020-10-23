3c3d2c891f68e27aed3a9f06d56a1def186cc7ed
kyu <qq950810@gmail.com>
2020-10-23 21:24:58 +0800

    feat: init

--- packages/shennong/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/qiuqi/Documents/github/kyuch4n.github.io/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','request',],
-});
-
-export { plugin };
--- packages/shennong/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/qiuqi/Documents/github/kyuch4n.github.io/node_modules/react-helmet';
