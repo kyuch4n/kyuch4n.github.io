a0f62e4df224f15cfffd483b2c53f43453a18238
kyu <qq950810@gmail.com>
2020-10-27 09:18:58 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,60 @@
-var VER = 'v1';
-var CACHE_NAME = 'kyu-sw' + VER;
-var urlsToCache = [];
+const VER = '1.0.0';
+const CACHE_NAME = 'KYU_SW_V' + VER;
 
+const URL_TO_CACHE = [
+  '/assets/home.jpg',
+  '/css/framework.css',
+  '/css/syntax.css',
+  '/css/bootstrap.css',
+  '/js/framework.js',
+  '/js/jquery.js',
+  '/js/bootstrap.js',
+];
 self.addEventListener('install', function(event) {
+  console.log('[ServiceWorker] Install');
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
+      return cache.addAll(URL_TO_CACHE);
     }),
   );
 });
 
+self.addEventListener('activate', function(event) {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList
+          .filter(function(key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function(key) {
+            console.log('[ServiceWorker] Removing old cache: ', key);
+            return caches.delete(key);
+          }),
+      );
+    }),
+  );
+
+  return self.clients.claim();
+});
+
+const DOMAIN_TO_IGNORE = ['baidu.com'];
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(res => {
       return (
         res ||
         fetch(event.request)
           .then(responese => {
-            const responeseClone = responese.clone();
-            caches.open(CACHE_NAME).then(cache => {
-              cache.put(event.request, responeseClone);
-            });
+            const url = event.request.url;
+            if (!DOMAIN_TO_IGNORE.find(i => url.indexOf(i) > -1)) {
+              console.log('[ServiceWorker] Add to cache: ', url);
+              const responeseClone = responese.clone();
+              caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, responeseClone);
+              });
+            }
             return responese;
           })
           .catch(err => {
