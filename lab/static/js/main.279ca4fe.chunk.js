c3d625d6afa580b0a20c224ec5ae2126f8ab0b5c
kyu <qq950810@gmail.com>
2020-11-03 23:19:57 +0800

    Updates

--- lab/static/js/main.279ca4fe.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@kyu/shennong"]=this["webpackJsonp@kyu/shennong"]||[]).push([[0],{10:function(e,n,t){},4:function(e,n,t){"use strict";t.r(n);var i=t(2),s=t(0),c=t.n(s),o=t(3),h=t.n(o);t(10);h.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)("div",{style:{width:"100vw",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontSize:"80px",fontWeight:600},children:"L A B"})}),document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.279ca4fe.chunk.js.map
